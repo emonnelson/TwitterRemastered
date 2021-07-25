ebdc4d371192334c0562ddc0819794c54dd1f691
emonnelson <39386877+emonnelson@users.noreply.github.com>
2021-07-25 19:58:44 -0400

    Create SplashScreen1.java

---
+++ SplashScreen1.java
@@ -0,0 +1,28 @@
+package com.codepath.apps.restclienttemplate;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent e = new Intent(SplashActivity.this, LoginActivity.class);
+                startActivity(e);
+                finish();
+            }
+
+        }, 3000);
+
+
+    }
+}
